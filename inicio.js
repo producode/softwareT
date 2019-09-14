9f9ad640e195c5daeebccb40bd7062d049f32c97
producode <produgraf2000@gmail.com>
2019-09-14 10:10:55 -0300

    se mejoro la interfaz de modificar

--- inicio.js
+++ inicio.js
@@ -13,7 +13,14 @@ app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(express.static(__dirname + '/public'));
 app.get('/',function(req,res){
-  res.sendFile(__dirname + "/");
+  mongoDB.connect(DataBase, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("baseT");
+    dbo.collection("almacen").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      res.render(__dirname + "/index.ejs",{documento: result});
+    });
+  });
 });
 app.get('/modificacionLibre',function(req,res){
   res.render(__dirname + "/paginas/modificar.ejs",{id: 0});
@@ -28,21 +35,64 @@ app.post('/modificar',function(Request,Response){
     var dbo = db.db("baseT");
     dbo.collection("almacen").find({}).toArray(function(err, result) {
       if (err) throw err;
-      result.result.forEach(element => {
+      result.forEach(element => {
         if(element.id == Request.body.Identificador){
-          var myquery = { id: Request.body.Identificador };
-          if(Request.body.Modificacion[Request.body.Modificacion.selected])
-          var newvalues = { $set: {name: "Mickey", address: "Canyon 123" } };
-          dbo.collection("customers").updateOne(myquery, newvalues, function(err, res) {
+          console.log(element);
+          var myquery = { id: parseInt(Request.body.Identificador) };
+          var cambiar = 0;
+          if(Request.body.Modificacion == "agregar"){
+            cambiar = parseInt(element.cantidad) + parseInt(Request.body.Cantidad);
+          }
+          else if(Request.body.Modificacion == "sacar"){
+            cambiar = parseInt(element.cantidad) - parseInt(Request.body.Cantidad);
+          }
+          console.log(cambiar);
+          var newvalues = { $set: { cantidad: cambiar,fechaModificacion: formatoFecha((new Date(Date.now()))) } };
+          dbo.collection("almacen").updateOne(myquery, newvalues, function(err, res) {
             if (err) throw err;
             console.log("1 document updated");
+            dbo.collection("almacen").find({}).toArray(function(err, result) {
+              if (err) throw err;
+              Response.render(__dirname + "/index.ejs",{documento: result});
+            });
           });
         }
       });
-      db.close();
     });
   });
 });
+app.post('/transferir',function(Request,Response){
+
+  mongoDB.connect(DataBase, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("baseT");
+    dbo.collection("almacen").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      result.forEach(element => {
+        if(element.id == Request.body.Identificador){
+          console.log(element);
+          var myquery = { id: parseInt(Request.body.Identificador) };
+          var newvalues = { $set: { sector: Request.body.Sector,fechaModificacion: formatoFecha(new Date(Date.now())) } };
+          dbo.collection("almacen").updateOne(myquery, newvalues, function(err, res) {
+            if (err) throw err;
+            console.log("1 document updated");
+            dbo.collection("almacen").find({}).toArray(function(err, result) {
+              if (err) throw err;
+              Response.render(__dirname + "/index.ejs",{documento: result});
+            });
+          });
+        }
+      });
+    });
+  });
+});
+function formatoFecha(fechaActual){
+  var minutos = fechaActual.getMinutes();
+  if(minutos < 10){
+    minutos = "0" + toString(minutos);
+  }
+  return (fechaActual.getDate() + '/' + fechaActual.getMonth() + '/' + fechaActual.getFullYear() + " " + fechaActual.getHours() + ":" + fechaActual.getMinutes());
+}
 app.listen(PuertoNodeJS, function() {
   console.log("Node server running on " + PuertoNodeJS);
 });
