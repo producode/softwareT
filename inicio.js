452306b04c15c49843e9794545f02f1f21c9ab5e
lautaro gonzalez <lautaro.gonzalez@siemens.com>
2019-09-19 14:02:31 -0300

    Requisito de autorizacion agregado

--- inicio.js
+++ inicio.js
@@ -29,14 +29,38 @@ app.get('/',function(req,res){
   });
 });
 app.get('/modificacionLibre',function(req,res){
-  res.render(__dirname + "/paginas/modificar.ejs",{id: 0});
+  if(req.session.loggedin && (req.session.acceso == "administracion" || req.session.acceso == "normal")){
+    res.render(__dirname + "/paginas/modificar.ejs",{id: 0});
+  }
+  else{
+    mongoDB.connect(DataBase, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("baseT");
+      dbo.collection("almacen").find({}).toArray(function(err, result) {
+        if (err) throw err;
+        res.render(__dirname + "/index.ejs",{documento: result});
+      });
+    });
+  }
 });
 app.post('/modificacionLink',function(Request,Response){
-  Response.render(__dirname + "/paginas/modificar.ejs",{id: Request.body.id});
+  if(Request.session.loggedin && (Request.session.acceso == "administracion" || Request.session.acceso == "normal")){
+    Response.render(__dirname + "/paginas/modificar.ejs",{id: Request.body.id});
+  }
+  else{
+    mongoDB.connect(DataBase, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("baseT");
+      dbo.collection("almacen").find({}).toArray(function(err, result) {
+        if (err) throw err;
+        Response.render(__dirname + "/index.ejs",{documento: result});
+      });
+    });
+  }
 });
 app.get('/ingresar',function(req,res){
   if(req.session.loggedin && req.session.acceso == "administracion"){
-  res.render(__dirname + "/paginas/ingresar.ejs",{alerta: "none"});
+    res.render(__dirname + "/paginas/ingresar.ejs",{alerta: "none"});
   }
   else{
     mongoDB.connect(DataBase, function(err, db) {
@@ -51,6 +75,19 @@ app.get('/ingresar',function(req,res){
 });
 app.get('/login',function(req,res){
   res.render(__dirname + "/paginas/login.ejs",{alerta:"none"});
+});
+app.get('/logout',function(req,res){
+  mongoDB.connect(DataBase, function(err, db) {
+    if (err) throw err;
+    req.session.loggedin = false;
+    req.session.usuario = "anonimo";
+    req.session.acceso = "anonimo";
+    var dbo = db.db("baseT");
+    dbo.collection("almacen").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      res.render(__dirname + "/index.ejs",{documento: result});
+    });
+  });
 });
 app.post('/loguearse',function(Request,Response){
   mongoDB.connect(DataBase,function(err,db){
@@ -98,7 +135,7 @@ app.post('/modificar',function(Request,Response){
             cambiar = parseInt(element.cantidad) - parseInt(Request.body.Cantidad);
           }
           console.log(cambiar);
-          var newvalues = { $set: { cantidad: cambiar,fechaModificacion: formatoFecha((new Date(Date.now()))),modificacion: tipoModificion } };
+          var newvalues = { $set: { cantidad: cambiar,fechaModificacion: formatoFecha((new Date(Date.now()))),modificacion: tipoModificion, editor: Request.session.usuario } };
           dbo.collection("almacen").updateOne(myquery, newvalues, function(err, res) {
             if (err) throw err;
             console.log("1 document updated");
@@ -123,7 +160,7 @@ app.post('/transferir',function(Request,Response){
         if(element.id == Request.body.Identificador){
           console.log(element);
           var myquery = { id: parseInt(Request.body.Identificador) };
-          var newvalues = { $set: { sector: Request.body.Sector,fechaModificacion: formatoFecha(new Date(Date.now())),modificacion: "transferencia" } };
+          var newvalues = { $set: { sector: Request.body.Sector,fechaModificacion: formatoFecha(new Date(Date.now())),modificacion: "transferencia", editor: Request.session.usuario } };
           dbo.collection("almacen").updateOne(myquery, newvalues, function(err, res) {
             if (err) throw err;
             console.log("1 document updated");
@@ -142,55 +179,43 @@ app.post('/ingresarObjeto',function(Request,Response){
   mongoDB.connect(DataBase, function(err, db) {
     if (err) throw err;
     var dbo = db.db("baseT");
-    if(Request.body.Contrasena == "T$producciones"){
-      dbo.collection("almacen").find({}).toArray(function(err, result) {
+    dbo.collection("almacen").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      var idNuevo = 0;
+      result.forEach(element => {
+        if(element.id > idNuevo){
+          idNuevo = element.id;
+        }
+      });
+      idNuevo = idNuevo + 1;
+      var myobj = { id: idNuevo, nombre: Request.body.Nombre, sector:Request.body.Sector, fechaModificacion: formatoFecha(new Date(Date.now())), editor: Request.session.usuario , modificacion: "ingreso", cantidad: Request.body.Cantidad , cantidadMinima: Request.body.CantidadMinima , unidad: Request.body.Unidad };
+      dbo.collection("almacen").insertOne(myobj, function(err, res) {
         if (err) throw err;
-        var idNuevo = 0;
-        result.forEach(element => {
-          if(element.id > idNuevo){
-            idNuevo = element.id;
-          }
-        });
-        idNuevo = idNuevo + 1;
-        var myobj = { id: idNuevo, nombre: Request.body.Nombre, sector:Request.body.Sector, fechaModificacion: formatoFecha(new Date(Date.now())), editor: "anonimo" , modificacion: "ingreso", cantidad: Request.body.Cantidad , cantidadMinima: Request.body.CantidadMinima , unidad: Request.body.Unidad };
-        dbo.collection("almacen").insertOne(myobj, function(err, res) {
+        console.log("1 document inserted");
+        dbo.collection("almacen").find({}).toArray(function(err, result) {
           if (err) throw err;
-          console.log("1 document inserted");
-          dbo.collection("almacen").find({}).toArray(function(err, result) {
-            if (err) throw err;
-            Response.render(__dirname + "/index.ejs",{documento: result});
-            db.close();
-          });
+          Response.render(__dirname + "/index.ejs",{documento: result});
+          db.close();
         });
       });
-    }
-    else{
-      Response.render(__dirname + "/paginas/ingresar.ejs",{alerta: "contraseña incorrecta"});
-      db.close();
-    }
+    });
   });
 });
 app.post('/eliminarObjeto',function(Request,Response){
     mongoDB.connect(DataBase, function(err, db) {
       if (err) throw err;
       var dbo = db.db("baseT");
-      if(Request.body.Contrasena == "T$producciones"){
-        var myquery = { id: parseInt(Request.body.Identificador) };
-        console.log(myquery);
-        dbo.collection("almacen").deleteOne(myquery, function(err, obj) {
+      var myquery = { id: parseInt(Request.body.Identificador) };
+      console.log(myquery);
+      dbo.collection("almacen").deleteOne(myquery, function(err, obj) {
+        if (err) throw err;
+        console.log("1 document deleted");
+        dbo.collection("almacen").find({}).toArray(function(err, result) {
           if (err) throw err;
-          console.log("1 document deleted");
-          dbo.collection("almacen").find({}).toArray(function(err, result) {
-            if (err) throw err;
-            Response.render(__dirname + "/index.ejs",{documento: result});
-            db.close();
-          });
+          Response.render(__dirname + "/index.ejs",{documento: result});
+          db.close();
         });
-      }
-      else{
-        Response.render(__dirname + "/paginas/ingresar.ejs",{alerta: "contraseña incorrecta"});
-        db.close();
-      }
+      });
     });
 });
 function formatoFecha(fechaActual){
